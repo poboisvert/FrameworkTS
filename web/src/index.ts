b67deb258c91cce59b1c963dc50380022aacb7c7
Pierre-Olivier Boisvert <pob944@hotmail.com>
2021-03-16 18:18:14 -0400

    First Model

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,1 +1,8 @@
-console.log("First");
+//console.log("First");
+
+import { User } from "./models/User";
+
+const user = new User({ name: "Name", age: 20 });
+
+console.log(user.get("name"));
+console.log(user.get("age"));
