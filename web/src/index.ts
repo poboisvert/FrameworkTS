9224b00301fde0afbaef7b0356d130e1b60a87a7
Pierre-Olivier Boisvert <pob944@hotmail.com>
2021-03-16 19:49:13 -0400

    Event

--- web/src/index.ts
+++ web/src/index.ts
@@ -4,5 +4,13 @@ import { User } from "./models/User";
 
 const user = new User({ name: "Name", age: 20 });
 
+user.set({ name: "newtest", age: 99 });
+
 console.log(user.get("name"));
 console.log(user.get("age"));
+
+user.on("change", () => {
+  console.log("Change 1");
+});
+
+console.log(user);
--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -1,12 +1,42 @@
 interface UserProps {
-  name: string;
-  age: number;
+  name?: string;
+  age?: number; // ? means optional
 }
 
+type Callback = () => void;
+
 export class User {
+  events: { [key: string]: Callback[] } = {};
+
   constructor(private data: UserProps) {}
 
+  // Get function
   get(propName: string): number | string {
     return this.data[propName];
+  }
+  // Set function
+  set(update: UserProps): void {
+    Object.assign(this.data, update); // From update to override this.data
+  }
+  //
+  on(eventName: string, callback: Callback): void {
+    //
+    const handlers = this.events[eventName] || [];
+    handlers.push(callback);
+    this.events[eventName] = handlers;
+  }
+
+  trigger(eventName: string): void {
+    const handlers = this.events[eventName];
+
+    // Check handlers
+    if (!handlers || handlers.length === 0) {
+      return;
+    }
+
+    // If handlers
+    handlers.forEach((callback) => {
+      callback();
+    });
   }
 }
