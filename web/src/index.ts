bade1156cfdd5d45b793afec1a8b7394f39c0b77
Pierre-Olivier Boisvert <pob944@hotmail.com>
2021-03-22 18:44:37 -0400

    List Users

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,16 +1,30 @@
-import { User } from "./models/User";
-import { UserEdit } from "./views/UserEdit";
+import { UserList } from "./views/UserList";
+import { Collection } from "./models/Collection";
+import { User, UserProps } from "./models/User";
 
-const user = User.buildUser({ name: "NAME1", age: 18 });
+const users = new Collection(
+  "http://localhost:3000/users",
+  (json: UserProps) => {
+    return User.buildUser(json);
+  }
+);
 
-const root = document.getElementById("root");
+users.on("change", () => {
+  const root = document.getElementById("root");
+
+  if (root) {
+    new UserList(root, users).render();
+  }
+});
+
+users.fetch();
 
 // Loading HTML
 
-const userEdit = new UserEdit(root, user);
+/* const userEdit = new UserEdit(root, user);
 userEdit.render(); // TSC config Strict null fix
 console.log(userEdit);
-
+ */
 /* const collection = User.buildUserCollection();
 
 collection.on("change", () => {
